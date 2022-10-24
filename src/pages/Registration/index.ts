69d7c8c2d0d7fdcdc4e970c02b5efc482d927753
dakryuchkov <dakryuchkov@phoenixit.ru>
2022-10-24 23:14:07 +0300

    sprint_2 add btn component to auth pages

--- src/pages/Registration/index.ts
+++ src/pages/Registration/index.ts
@@ -1,8 +1,11 @@
 import Registration from "./registration";
 import {renderDom} from "../../utils/renderDom";
+import Button from "../../components/button/button";
 
 document.addEventListener('DOMContentLoaded', () => {
-   let registrationPage = new Registration({});
+   let registrationPage = new Registration({
+      button: new Button({ text: "Register" })
+   });
 
    renderDom(registrationPage);
 });
